6a8b3686a8adce3656fea2e96ad6a4dbf6e739b3
Relty11812 <relty.works@gmail.com>
2025-09-11 09:41:21 +0900

    250911_A
    <복습시작>
    퀴즈퀴즈

--- 0729_03/obj/Debug/net9.0/0729_03.AssemblyInfo.cs
+++ 0729_03/obj/Debug/net9.0/0729_03.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("0729_03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6333868469bf93aa48db45bb1a1b0806da1bdaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9825953a917df325d9f93af8fe3cf6cdee0cde8")]
 [assembly: System.Reflection.AssemblyProductAttribute("0729_03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("0729_03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 0911_01/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _0911_01 {
+
+    class Quiz {
+        public void Swap(ref int _x, ref int _y) {
+            int temp = _x;
+            _x = _y;
+            _y = temp;
+        }
+    }
+
+    internal class Program {
+        static void Main(string[] args) {
+            Quiz quiz = new Quiz();
+
+            int x = 10;
+            int y = 20;
+            quiz.Swap(ref x, ref y);
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+}
